99a740d35da1e5740bfa3326eb7e9f2ff51d3e8d
Mohamed Amine Nasfi <152500239+medaminenasfi@users.noreply.github.com>
2025-04-07 21:03:56 +0100

    wsd

--- models/Circuit.js
+++ models/Circuit.js
@@ -4,7 +4,7 @@ const CircuitSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     location: { type: String, required: true },
-    duration: { type: Number, required: true }, // Duration in hours
+    duration: { type: Number, required: true }, 
     price: { type: Number, required: true },
     difficulty: { type: String, enum: ["Facile", "Moyen", "Difficile"], required: true },
     createdAt: { type: Date, default: Date.now }
